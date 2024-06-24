eee709a2a8cd1ff2cc14722dfd53345114a34a93
yashindows <kiram.imav@gmal.com>
2024-06-25 00:03:17 +0900

    animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,14 @@
 const btn = document.querySelector(".btn")
 const hamster = document.querySelector(".content-image")
 const title = document.querySelector(".title")
+const loader = document.querySelector(".loader")
 
 let counter = 1
+
+window.addEventListener("load", (e) => {
+  loader.style.display = "none"
+  btn.style.display = "inline-block"
+})
 
 btn.addEventListener("click", () => {
   hamster.style.display = "block"
@@ -12,5 +18,9 @@ btn.addEventListener("click", () => {
 
 hamster.addEventListener("click", () => {
   ++counter
+  hamster.style.scale = 0.9
   title.innerText = `Счет: ${counter}`
+  setTimeout(() => {
+    hamster.style.scale = 1
+  }, 50)
 })
