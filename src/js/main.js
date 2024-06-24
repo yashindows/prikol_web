2c27ab2c9b61666821f5591c929abc93b239d8bf
yashindows <kiram.imav@gmal.com>
2024-06-25 01:00:44 +0900

    delete btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,21 @@
-const btn = document.querySelector(".btn")
 const hamster = document.querySelector(".content-image")
 const title = document.querySelector(".title")
 const loader = document.querySelector(".loader")
 
-let counter = 1
+if (!localStorage.counter) {
+  localStorage.setItem("counter", 1)
+}
 
 window.addEventListener("load", (e) => {
   loader.style.display = "none"
-  btn.style.display = "inline-block"
 })
 
-btn.addEventListener("click", () => {
-  hamster.style.display = "block"
-  btn.style.display = "none"
-  title.innerText = `Счет: ${counter}`
-})
+title.innerText = `Счет: ${localStorage.counter}`
 
 hamster.addEventListener("click", () => {
-  ++counter
+  ++localStorage.counter
   hamster.style.scale = 0.9
-  title.innerText = `Счет: ${counter}`
+  title.innerText = `Счет: ${localStorage.counter}`
   setTimeout(() => {
     hamster.style.scale = 1
   }, 50)
